c9d39de41a299a21d9d49d1dea1e5f7c774897ce
aydanmrs <164398449+aydanmrs@users.noreply.github.com>
2024-08-12 22:03:51 +0400

    main activity commit

--- app/src/main/java/com/example/branchapp/MainActivity.kt
+++ app/src/main/java/com/example/branchapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.branchapp
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.branchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.mainactivity_bg)
+        }
+
+        binding.memoryGame.setOnClickListener {
+            val intent = Intent(this@MainActivity, StartActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
