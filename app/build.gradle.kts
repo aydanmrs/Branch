d59a3a2e0353261b66a212f88a8c4e3fb52887b6
aydanmrs <164398449+aydanmrs@users.noreply.github.com>
2024-08-12 19:03:44 +0400

    start activity 1 xml commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.branchapp"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding=true
+    }
+
     defaultConfig {
         applicationId = "com.example.branchapp"
         minSdk = 24
--- app/src/main/java/com/example/branchapp/StartActivity1.kt
+++ app/src/main/java/com/example/branchapp/StartActivity1.kt
@@ -1,11 +1,30 @@
 package com.example.branchapp
 
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.branchapp.databinding.ActivityStart1Binding
 
 class StartActivity1 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStart1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start1)
+        binding=ActivityStart1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
+        binding.startButton.setOnClickListener {
+            val intent = Intent(this, TicTacToeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
