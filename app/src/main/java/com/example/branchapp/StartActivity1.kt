86c72e2a1d9efcc6f99cba394185eb2cfb43b172
aydanmrs <164398449+aydanmrs@users.noreply.github.com>
2024-08-12 18:57:49 +0400

    start activity 1 xml commit

---
+++ app/src/main/java/com/example/branchapp/StartActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.branchapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start1)
+    }
+}
