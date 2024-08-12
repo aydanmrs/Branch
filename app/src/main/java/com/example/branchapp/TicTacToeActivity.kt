c9bbf6efaf219077ec736cd3ee22f5ebf9eeb6ef
aydanmrs <164398449+aydanmrs@users.noreply.github.com>
2024-08-12 19:19:17 +0400

    TicTacToe xml commit

--- app/src/main/java/com/example/branchapp/TicTacToeActivity.kt
+++ app/src/main/java/com/example/branchapp/TicTacToeActivity.kt
@@ -1,11 +1,25 @@
 package com.example.branchapp
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.branchapp.databinding.ActivityTicTacToeBinding
 
 class TicTacToeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTicTacToeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tic_tac_toe)
+        binding=ActivityTicTacToeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.statusBarColor = Color.TRANSPARENT
+        }
     }
 }
